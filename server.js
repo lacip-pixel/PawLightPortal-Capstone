9aeb1f32021c34819f9c5dcb149ce5eab79c8dd6
lukelynch10 <llynch10.4242@gmail.com>
2025-01-29 12:47:05 -0500

    basic skeleton of homepage, please install node js and express team

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Define a simple route
 app.get("/", (req, res) => {
-    res.send("Hello, Express!");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 // Set the server to listen on a port
 const PORT = 3000;
 app.listen(PORT, () => {
