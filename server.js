09d5eca796e35bd0353d7f4d3f67bc790a8a1c26
lukelynch10 <llynch10.4242@gmail.com>
2025-04-07 11:35:29 -0400

    changed to port 80

--- server.js
+++ server.js
@@ -157,9 +157,9 @@ app.get("/get-user-plan", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on Port:${PORT}`);
 });
 
 // Simulated API endpoint for door controls
