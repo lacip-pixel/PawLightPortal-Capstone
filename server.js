0ddc8a9cf9a7790bd00afd696c03714979f9ccb3
lukelynch10 <llynch10.4242@gmail.com>
2025-04-10 15:07:36 -0400

    fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config({ path: "./.env" });
 const pool = new Pool({
     user: process.env.DATABASE_USER || "postgres",
     host: process.env.DATABASE_HOST || "localhost",
-    database: process.env.DATABASE || "PawlightPortalLocal",
+    database: process.env.DATABASE || "PawlightPortal",
     password: process.env.DATABASE_PASSWORD || "postgres",
     port: process.env.DATABASE_PORT || 5432,
 });
